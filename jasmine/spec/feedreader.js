26109bd2b840ee301e7c02d93e6d6f54536bbf98
Sara Krum <sarasmith887@gmail.com>
2018-07-18 18:57:38 -0400

    spec added - body expected to have class .menu-hidden

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -54,14 +54,13 @@ $(function() {
 
     /* TODO: Write a new test suite named "The menu" */
 	describe('The menu', () => {
-		let body = document.querySelector('body');
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
-        it('Menu toggle', () =>{
-			expect(body).toEqual(jasmine.any('menu-hidden'));
+        it('Menu toggle', () => {
+			expect(document.querySelector('body')).toHaveClass('menu-hidden');
         });
 
          /* TODO: Write a test that ensures the menu changes
