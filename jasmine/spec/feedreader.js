ea5e0c7c327a7f9b94332176b8baca434ff51430
Sara Krum <sarasmith887@gmail.com>
2018-07-18 18:41:39 -0400

    jasmine lib updated to current version

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -53,12 +53,16 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "The menu" */
-
+	describe('The menu', () => {
+		let body = document.querySelector('body');
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+        it('Menu toggle', () =>{
+			expect(body).toEqual(jasmine.any('menu-hidden'));
+        });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
@@ -81,4 +85,5 @@ $(function() {
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+	});
 }());
